521a8b1fe830f2b9c7228ea332b782bc76ac982f
senami <semanad@hotmail.com>
2014-08-21 13:09:16 +0000

    Update cachematrix.R
    ASSIGNMENT 2 R PROGRAMMING

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+## This function make a matrix object and calculate the inverse if it's a 
+## square matrix. The inverse is cached. 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(mat = matrix(numeric())){
+  if((nrow(mat)!=ncol(mat))||(det(mat)==0)){
+    print("This matrix is not inversible")
+  }
+  else{
+    matInvers<-NULL
+    set<- function(matInit){
+      if((nrow(matInit)!=ncol(matInit))||(det(matInit)==0)){
+        print("This matrix is not inversible")
+      }
+      else{
+        mat<<- matInit
+        matInvers<<- NULL      
+      }
+    }
+    get<- function() mat
+    invmat<- solve(mat)
+    setInvers<- function(invmat){
+      matInvers <<- invmat
+    }
+    getInvers<- function(){
+      matInvers
+    }
+    list(set=set,get=get,setInvers=setInvers,getInvers=getInvers)
+    
+  }
+ 
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function return the inverse of a matrix, before calculate
+## the inverse it surch the inverse in the cache. If it's not in 
+## the environnement the inverse is return
+cacheSolve <- function(mat, ...) {
+  ## Return a matrix that is the inverse of 'mat'
+  matInvers <- mat$getInvers()
+  if(!is.null(matInvers)){
+    print("getting cached data")
+    return(matInvers)
+  }
+  data<- mat$get()
+  matInvers<- solve(data)
+  mat$setInvers(matInvers)
+  matInvers
+  
 }
